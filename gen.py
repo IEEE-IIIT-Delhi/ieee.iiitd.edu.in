8556b6e03add3265d252503b0ca677056cd6ef93
sc0rp10n-w11lappy <pragyan52yadav@gmail.com>
2022-01-26 19:33:20 +0530

    members, mem info, scripts

---
+++ gen.py
@@ -0,0 +1,319 @@
+HTMLString = """
+
+"""
+
+data = {
+    "rep" : {
+        "0" : {
+            "name": "Prof. S. S. Jamuar",
+            "pos": "Faculty Coordinator",
+            "img": "https://via.placeholder.com/100x100",
+            "social": {
+                "fb": "https://www.facebook.com/",
+                "tw": "https://twitter.com/",
+                "wb": "#"
+            },
+            "desc": "lorem"
+        },
+        "1" : {
+            "name": "Dr. Shobha Sundar Ram",
+            "pos": "Head of Department, ECE",
+            "img": "https://via.placeholder.com/100x100",
+            "social": {
+                "fb": "https://www.facebook.com/",
+                "tw": "https://twitter.com/",
+                "wb": "#"
+            },
+            "desc": "lorem"
+        },
+        "2" : {
+            "name": "Dr. Rajiv Ratn Shah",
+            "pos": "CompSoc Faculty Coordinator",
+            "img": "https://via.placeholder.com/100x100",
+            "social": {
+                "fb": "https://www.facebook.com/",
+                "tw": "https://twitter.com/",
+                "wb": "#"
+            },
+            "desc": "lorem"
+        }
+    },
+    "core": {
+        "3" : {
+            "name": "Prakhar Prasad",
+            "pos": "Chairperson",
+            "img": "https://via.placeholder.com/100x100",
+            "social": {
+                "fb": "https://www.facebook.com/",
+                "tw": "https://twitter.com/",
+                "wb": "#"
+            },
+            "desc": "lorem"
+        },
+        "4" : {
+            "name": "Ansh Arora",
+            "pos": "Vice Chairperson",
+            "img": "https://via.placeholder.com/100x100",
+            "social": {
+                "fb": "https://www.facebook.com/",
+                "tw": "https://twitter.com/",
+                "wb": "#"
+            },
+            "desc": "lorem"
+        },
+        "5" : {
+            "name": "Gursimran Kaur",
+            "pos": "Secretary / Coordinator",
+            "img": "https://via.placeholder.com/100x100",
+            "social": {
+                "fb": "https://www.facebook.com/",
+                "tw": "https://twitter.com/",
+                "wb": "#"
+            },
+            "desc": "lorem"
+        },
+        "6" : {
+            "name": "Garmit Pant",
+            "pos": "Treasurer / Coordinator",
+            "img": "https://via.placeholder.com/100x100",
+            "social": {
+                "fb": "https://www.facebook.com/",
+                "tw": "https://twitter.com/",
+                "wb": "#"
+            },
+            "desc": "lorem"
+        },
+        "7" : {
+            "name": "Mihir Chaturvedi",
+            "pos": "WebMaster",
+            "img": "https://via.placeholder.com/100x100",
+            "social": {
+                "fb": "https://www.facebook.com/",
+                "tw": "https://twitter.com/",
+                "wb": "#"
+            },
+            "desc": "lorem"
+        },
+        "8" : {
+            "name": "Daksh T",
+            "pos": "Industrial Outreach",
+            "img": "https://via.placeholder.com/100x100",
+            "social": {
+                "fb": "https://www.facebook.com/",
+                "tw": "https://twitter.com/",
+                "wb": "#"
+            },
+            "desc": "lorem"
+        },
+        "9" : {
+            "name": "Sachleen Kaur",
+            "pos": "Design Head",
+            "img": "https://via.placeholder.com/100x100",
+            "social": {
+                "fb": "https://www.facebook.com/",
+                "tw": "https://twitter.com/",
+                "wb": "#"
+            },
+            "desc": "lorem"
+        },
+        "10" : {
+            "name": "Shragvi Sidarth Jha",
+            "pos": "Operations Head",
+            "img": "https://via.placeholder.com/100x100",
+            "social": {
+                "fb": "https://www.facebook.com/",
+                "tw": "https://twitter.com/",
+                "wb": "#"
+            },
+            "desc": "lorem"
+        },
+        "11" : {
+            "name": "Niranjan Sundarajan",
+            "pos": "Events Head",
+            "img": "https://via.placeholder.com/100x100",
+            "social": {
+                "fb": "https://www.facebook.com/",
+                "tw": "https://twitter.com/",
+                "wb": "#"
+            },
+            "desc": "lorem"
+        }
+    },
+    "wie": {
+        "12" : {
+            "name": "Meetakshi Setiya",
+            "pos": "WiE Chairperson / Coordinator",
+            "img": "https://via.placeholder.com/100x100",
+            "social": {
+                "fb": "https://www.facebook.com/",
+                "tw": "https://twitter.com/",
+                "wb": "#"
+            },
+            "desc": "lorem"
+        },
+        "13" : {
+            "name": "Amolika Bansal",
+            "pos": "Vice Chairperson",
+            "img": "https://via.placeholder.com/100x100",
+            "social": {
+                "fb": "https://www.facebook.com/",
+                "tw": "https://twitter.com/",
+                "wb": "#"
+            },
+            "desc": "lorem"
+        },
+        "14" : {
+            "name": "Sunishka Sharma",
+            "pos": "Secretary",
+            "img": "https://via.placeholder.com/100x100",
+            "social": {
+                "fb": "https://www.facebook.com/",
+                "tw": "https://twitter.com/",
+                "wb": "#"
+            },
+            "desc": "lorem"
+        }
+    },
+    "compsoc": {
+        "15" : {
+            "name": "Rohan Dhar",
+            "pos": "CompSoc Chairperson",
+            "img": "https://via.placeholder.com/100x100",
+            "social": {
+                "fb": "https://www.facebook.com/",
+                "tw": "https://twitter.com/",
+                "wb": "#"
+            },
+            "desc": "lorem"
+        },
+        "16" : {
+            "name": "Abhik S Basu",
+            "pos": "Vice Chairperson",
+            "img": "https://via.placeholder.com/100x100",
+            "social": {
+                "fb": "https://www.facebook.com/",
+                "tw": "https://twitter.com/",
+                "wb": "#"
+            },
+            "desc": "lorem"
+        }
+    },
+    "exec": {
+        "17" : {
+            "name": "Raghav Sahni",
+            "pos": "CMO",
+            "img": "https://via.placeholder.com/100x100",
+            "social": {
+                "fb": "https://www.facebook.com/",
+                "tw": "https://twitter.com/",
+                "wb": "#"
+            },
+            "desc": "lorem"
+        },
+        "18" : {
+            "name": "Tejdeep Ch",
+            "pos": "CO",
+            "img": "https://via.placeholder.com/100x100",
+            "social": {
+                "fb": "https://www.facebook.com/",
+                "tw": "https://twitter.com/",
+                "wb": "#"
+            },
+            "desc": "lorem"
+        },
+        "19" : {
+            "name": "Anusha Tiwari",
+            "pos": "CMO",
+            "img": "https://via.placeholder.com/100x100",
+            "social": {
+                "fb": "https://www.facebook.com/",
+                "tw": "https://twitter.com/",
+                "wb": "#"
+            },
+            "desc": "lorem"
+        },
+        "20" : {
+            "name": "Ritwick Pal",
+            "pos": "CMO",
+            "img": "https://via.placeholder.com/100x100",
+            "social": {
+                "fb": "https://www.facebook.com/",
+                "tw": "https://twitter.com/",
+                "wb": "#"
+            },
+            "desc": "lorem"
+        },
+        "21" : {
+            "name": "Jogith S Chandran",
+            "pos": "CO",
+            "img": "https://via.placeholder.com/100x100",
+            "social": {
+                "fb": "https://www.facebook.com/",
+                "tw": "https://twitter.com/",
+                "wb": "#"
+            },
+            "desc": "lorem"
+        },
+        "22" : {
+            "name": "Samyak Jain",
+            "pos": "CMO",
+            "img": "https://via.placeholder.com/100x100",
+            "social": {
+                "fb": "https://www.facebook.com/",
+                "tw": "https://twitter.com/",
+                "wb": "#"
+            },
+            "desc": "lorem"
+        },
+        "23" : {
+            "name": "Mohit Jain",
+            "pos": "CMO",
+            "img": "https://via.placeholder.com/100x100",
+            "social": {
+                "fb": "https://www.facebook.com/",
+                "tw": "https://twitter.com/",
+                "wb": "#"
+            },
+            "desc": "lorem"
+        },
+        "24" : {
+            "name": "Aayush Kapoor",
+            "pos": "CO",
+            "img": "https://via.placeholder.com/100x100",
+            "social": {
+                "fb": "https://www.facebook.com/",
+                "tw": "https://twitter.com/",
+                "wb": "#"
+            },
+            "desc": "lorem"
+        }
+    },
+    "alum": {
+        "0" : {
+            "name": "Max",
+            "pos": "CMO",
+            "img": "https://via.placeholder.com/100x100",
+            "social": {
+                "fb": "https://www.facebook.com/",
+                "tw": "https://twitter.com/",
+                "wb": "#"
+            },
+            "desc": "lorem"
+        },
+        "1" : {
+            "name": "Jack",
+            "pos": "CO",
+            "img": "https://via.placeholder.com/100x100",
+            "social": {
+                "fb": "https://www.facebook.com/",
+                "tw": "https://twitter.com/",
+                "wb": "#"
+            },
+            "desc": "lorem"
+        }
+    }
+}
+
+for i in range(len(data)):
+    
+
--- members.js
+++ members.js
@@ -1,74 +1,166 @@
 const data = {
     rep : {
         0 : {
-            name: "Liam",
-            pos: "CEO",
-            img: "https://via.placeholder.com/100x100"
+            name: "Prof. S. S. Jamuar",
+            pos: "Faculty Coordinator",
+            img: "https://via.placeholder.com/100x100",
+            a: "./members/1.html"
         },
         1 : {
-            name: "Beck",
-            pos: "COO",
-            img: "https://via.placeholder.com/100x100"
+            name: "Dr. Shobha Sundar Ram",
+            pos: "Head of Department, ECE",
+            img: "https://via.placeholder.com/100x100",
+            a: "./members/2.html"
+        },
+        2 : {
+            name: "Dr. Rajiv Ratn Shah",
+            pos: "CompSoc Faculty Coordinator",
+            img: "https://via.placeholder.com/100x100",
+            a: "./members/3.html"
         }
     },
     core: {
-        0 : {
-            name: "Max",
-            pos: "CMO",
-            img: "https://via.placeholder.com/100x100"
+        3 : {
+            name: "Prakhar Prasad",
+            pos: "Chairperson",
+            img: "https://via.placeholder.com/100x100",
+            a: "./members/4.html"
         },
-        1 : {
-            name: "Jack",
-            pos: "CO",
-            img: "https://via.placeholder.com/100x100"
+        4 : {
+            name: "Ansh Arora",
+            pos: "Vice Chairperson",
+            img: "https://via.placeholder.com/100x100",
+            a: "./members/5.html"
+        },
+        5 : {
+            name: "Gursimran Kaur",
+            pos: "Secretary / Coordinator",
+            img: "https://via.placeholder.com/100x100",
+            a: "./members/6.html"
+        },
+        6 : {
+            name: "Garmit Pant",
+            pos: "Treasurer / Coordinator",
+            img: "https://via.placeholder.com/100x100",
+            a: "./members/7.html"
+        },
+        7 : {
+            name: "Mihir Chaturvedi",
+            pos: "WebMaster",
+            img: "https://via.placeholder.com/100x100",
+            a: "./members/8.html"
+        },
+        8 : {
+            name: "Daksh T",
+            pos: "Industrial Outreach",
+            img: "https://via.placeholder.com/100x100",
+            a: "./members/9.html"
+        },
+        9 : {
+            name: "Sachleen Kaur",
+            pos: "Design Head",
+            img: "https://via.placeholder.com/100x100",
+            a: "./members/10.html"
+        },
+        10 : {
+            name: "Shragvi Sidarth Jha",
+            pos: "Operations Head",
+            img: "https://via.placeholder.com/100x100",
+            a: "./members/11.html"
+        },
+        11 : {
+            name: "Niranjan Sundarajan",
+            pos: "Events Head",
+            img: "https://via.placeholder.com/100x100",
+            a: "./members/12.html"
         }
     },
     wie: {
-        0 : {
-            name: "Max",
-            pos: "CMO",
-            img: "https://via.placeholder.com/100x100"
+        12 : {
+            name: "Meetakshi Setiya",
+            pos: "WiE Chairperson / Coordinator",
+            img: "https://via.placeholder.com/100x100",
+            a: "./members/13.html"
         },
-        1 : {
-            name: "Jack",
-            pos: "CO",
-            img: "https://via.placeholder.com/100x100"
+        13 : {
+            name: "Amolika Bansal",
+            pos: "Vice Chairperson",
+            img: "https://via.placeholder.com/100x100",
+            a: "./members/14.html"
+        },
+        14 : {
+            name: "Sunishka Sharma",
+            pos: "Secretary",
+            img: "https://via.placeholder.com/100x100",
+            a: "./members/15.html"
         }
     },
     compsoc: {
-        0 : {
-            name: "Max",
-            pos: "CMO",
-            img: "https://via.placeholder.com/100x100"
+        15 : {
+            name: "Rohan Dhar",
+            pos: "CompSoc Chairperson",
+            img: "https://via.placeholder.com/100x100",
+            a: "./members/16.html"
         },
-        1 : {
-            name: "Jack",
-            pos: "CO",
-            img: "https://via.placeholder.com/100x100"
+        16 : {
+            name: "Abhik S Basu",
+            pos: "Vice Chairperson",
+            img: "https://via.placeholder.com/100x100",
+            a: "./members/17.html"
         }
     },
     exec: {
-        0 : {
-            name: "Max",
-            pos: "CMO",
-            img: "https://via.placeholder.com/100x100"
+        17 : {
+            name: "Raghav Sahni",
+            img: "https://via.placeholder.com/100x100",
+            a: "./members/18.html"
         },
-        1 : {
-            name: "Jack",
-            pos: "CO",
-            img: "https://via.placeholder.com/100x100"
+        18 : {
+            name: "Tejdeep Ch",
+            img: "https://via.placeholder.com/100x100",
+            a: "./members/19.html"
+        },
+        19 : {
+            name: "Anusha Tiwari",
+            img: "https://via.placeholder.com/100x100",
+            a: "./members/20.html"
+        },
+        20 : {
+            name: "Ritwick Pal",
+            img: "https://via.placeholder.com/100x100",
+            a: "./members/21.html"
+        },
+        21 : {
+            name: "Jogith S Chandran",
+            img: "https://via.placeholder.com/100x100",
+            a: "./members/22.html"
+        },
+        22 : {
+            name: "Samyak Jain",
+            img: "https://via.placeholder.com/100x100",
+            a: "./members/23.html"
+        },
+        23 : {
+            name: "Mohit Jain",
+            img: "https://via.placeholder.com/100x100",
+            a: "./members/24.html"
+        },
+        24 : {
+            name: "Aayush Kapoor",
+            img: "https://via.placeholder.com/100x100",
+            a: "./members/25.html"
         }
     },
     alum: {
-        0 : {
+        25 : {
             name: "Max",
-            pos: "CMO",
-            img: "https://via.placeholder.com/100x100"
+            img: "https://via.placeholder.com/100x100",
+            a: "./members/26.html"
         },
-        1 : {
+        26 : {
             name: "Jack",
-            pos: "CO",
-            img: "https://via.placeholder.com/100x100"
+            img: "https://via.placeholder.com/100x100",
+            a: "./members/27.html"
         }
     }
 }
@@ -84,62 +176,61 @@ const Exec = document.getElementById("exec");
 const Alum = document.getElementById("alum");
 
 let i;
-for (i = 0; i < Object.keys(data.rep).length; i++) {
-    Rep.innerHTML += `
-    <div class="flex flex-col items-center justify-center space-y-4">
+for (i = 0; i < 3; i++) {
+    Rep.innerHTML += `<a href="${data.rep[i].a}">
+    <div class="memCard flex flex-col items-center justify-center space-y-4">
         <img src="${data.rep[i].img}" alt="${data.rep[i].name}" class="pic">
         <h1 class="name">${data.rep[i].name}</h1>
         <h2 class="pos">${data.rep[i].pos}</h2>
-    </div>
+    </div></a>
     `
 }
 
-for (i = 0; i < Object.keys(data.core).length; i++) {
-    Core.innerHTML += `
-    <div class="flex flex-col items-center justify-center space-y-4">
+for (i = 3; i < 12; i++) {
+    Core.innerHTML += `<a href="${data.core[i].a}">
+    <div class="memCard flex flex-col items-center justify-center space-y-4">
         <img src="${data.core[i].img}" alt="${data.core[i].name}" class="pic">
         <h1 class="name">${data.core[i].name}</h1>
         <h2 class="pos">${data.core[i].pos}</h2>
-    </div>
+    </div></a>
     `
 }
 
-for (i = 0; i < Object.keys(data.wie).length; i++) {
-    Wie.innerHTML += `
-    <div class="flex flex-col items-center justify-center space-y-4">
+for (i = 12; i < 15; i++) {
+    Wie.innerHTML += `<a href="${data.wie[i].a}">
+    <div class="memCard flex flex-col items-center justify-center space-y-4">
         <img src="${data.wie[i].img}" alt="${data.wie[i].name}" class="pic">
         <h1 class="name">${data.wie[i].name}</h1>
         <h2 class="pos">${data.wie[i].pos}</h2>
-    </div>
+    </div></a>
     `
 }
 
-for (i = 0; i < Object.keys(data.compsoc).length; i++) {
-    Compsoc.innerHTML += `
-    <div class="flex flex-col items-center justify-center space-y-4">
+for (i = 15; i < 17; i++) {
+    Compsoc.innerHTML += `<a href="${data.compsoc[i].a}">
+    <div class="memCard flex flex-col items-center justify-center space-y-4">
         <img src="${data.compsoc[i].img}" alt="${data.compsoc[i].name}" class="pic">
         <h1 class="name">${data.compsoc[i].name}</h1>
         <h2 class="pos">${data.compsoc[i].pos}</h2>
-    </div>
+    </div></a>
     `
 }
 
-for (i = 0; i < Object.keys(data.exec).length; i++) {
-    Exec.innerHTML += `
-    <div class="flex flex-col items-center justify-center space-y-4">
+for (i = 17; i < 25; i++) {
+    Exec.innerHTML += `<a href="${data.exec[i].a}">
+    <div class="memCard flex flex-col items-center justify-center space-y-4">
         <img src="${data.exec[i].img}" alt="${data.exec[i].name}" class="pic">
         <h1 class="name">${data.exec[i].name}</h1>
-        <h2 class="pos">${data.exec[i].pos}</h2>
-    </div>
+    </div></a>
     `
 }
 
-for (i = 0; i < Object.keys(data.alum).length; i++) {
-    Alum.innerHTML += `
-    <div class="flex flex-col items-center justify-center space-y-4">
+for (i = 25; i < 27; i++) {
+    Alum.innerHTML += `<a href="${data.alum[i].a}">
+    <div class="memCard flex flex-col items-center justify-center space-y-4">
         <img src="${data.alum[i].img}" alt="${data.alum[i].name}" class="pic">
         <h1 class="name">${data.alum[i].name}</h1>
         <h2 class="pos">${data.alum[i].pos}</h2>
-    </div>
+    </div></a>
     `
 }
