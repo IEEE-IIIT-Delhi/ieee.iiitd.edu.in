4397d9b634497b623404948244ee75d3111091bb
Jaideep <guntupallijaideep@gmail.com>
2022-04-12 04:50:49 +0530

    added alumni

--- members.js
+++ members.js
@@ -59,7 +59,7 @@ fetch("members.json")
             `;
         }
 
-        for (i = 59; i < 61; i++) {
+        for (i = 59; i < 65; i++) {
             Alum.innerHTML += `
             <div class="memCard flex flex-col items-center justify-center space-y-4">
                 <img src="${data[i].img}" alt="${data[i].name}" class="pic">
