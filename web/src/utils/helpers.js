ca0ec63badd7aa0d30fde18136fca32a240f8568
arhaanb <arhaanb@gmail.com>
2024-01-15 14:21:54 +0530

    new members

--- web/src/utils/helpers.js
+++ web/src/utils/helpers.js
@@ -1,24 +1,21 @@
 import { parseISO, format } from "date-fns";
 
 export async function getSanityContent({ query, variables = {} }) {
-    const { data } = await fetch(
-        `${import.meta.env.PUBLIC_SANITY_GRAPHQL_URL}`,
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                query,
-                variables,
-            }),
-        }
-    ).then((response) => response.json());
-    return data;
+  const { data } = await fetch(`${import.meta.env.PUBLIC_SANITY_GRAPHQL_URL}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  }).then((response) => response.json());
+  return data;
 }
 
 export function formatBlogPostDate(date) {
-    const dateString = parseISO(date, "YYYY/MM/dd");
-    const formattedDateString = format(dateString, "dd MMMM yyyy");
-    return `${formattedDateString}`;
+  const dateString = parseISO(date, "YYYY/MM/dd");
+  const formattedDateString = format(dateString, "dd MMMM yyyy");
+  return `${formattedDateString}`;
 }
